56c3551fd5953fc5e554a70573b0e02945e04d54
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-03-14 18:49:42 +0000

    Created the constructor ready to use for creating the BST's used for testing

--- ASE-CW2/src/Lib.hs
+++ ASE-CW2/src/Lib.hs
@@ -3,12 +3,27 @@
 module Lib where
 import Control.Exception (BlockedIndefinitelyOnMVar)
 
-data BST item = InternalNode Int item (BST item) (BST item) | Leaf
+data BST item =
+  Node Int item (BST item) (BST item)
+  | Leaf
+  deriving (Show)
 
-bst_lookup :: Int -> BST item -> Maybe item
-bst_lookup soughtKey Leaf = Nothing
-bst_lookup soughtKey (InternalNode key item leftChild rightChild)
-  -- | soughtKey == Leaf = Leaf -- Is this needed? 
-  | soughtKey > key = bst_lookup soughtKey rightChild
-  | soughtKey < key = bst_lookup soughtKey leftChild
-  | otherwise = Just item -- If keys match
+bst_constructor :: BST String
+bst_constructor =
+  Node 20 "Eve"
+    (Node 10 "Ryder"
+      (Node 5 "Alex" Leaf Leaf)
+      (Node 15 "Dom" Leaf Leaf)
+    )
+    (Node 30 "Harry"
+      (Node 25 "Neil" Leaf Leaf)
+      (Node 35 "I need more friends" Leaf Leaf)
+    )
+
+-- bst_lookup :: Int -> BST item -> Maybe item
+-- bst_lookup soughtKey Leaf = Nothing
+-- bst_lookup soughtKey (Node key item leftChild rightChild)
+--   -- | soughtKey == Leaf = Leaf -- Is this needed? 
+--   | soughtKey > key = bst_lookup soughtKey rightChild
+--   | soughtKey < key = bst_lookup soughtKey leftChild
+--   | otherwise = Just item -- If keys match
