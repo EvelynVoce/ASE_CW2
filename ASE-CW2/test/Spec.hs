3ae7a1a92c5065182a909d6df80d5e500e92bdaa
EvelynVoce <jamievoce0115@gmail.com>
2022-03-17 13:32:01 +0000

    Updated the tests so that the expected string is a maybe string (this makes the test pass)

--- ASE-CW2/test/Spec.hs
+++ ASE-CW2/test/Spec.hs
@@ -5,21 +5,18 @@ import Lib
 
 main :: IO ()
 main = do
-  results <- runTestTT allTests
+  results <- runTestTT lookupTests
   print results
+
 
 -- Hard Coded BST implementation (Really need to change this at some point)
 bst_constructor :: BST String
 bst_constructor =
   Node 20 "Eve"
     (Node 10 "Ryder" (Node 5 "Alex" Leaf Leaf) (Node 15 "Dom" Leaf Leaf) )
-    (Node 30 "Harry" (Node 25 "Neil" Leaf Leaf) (Node 35 "I need more friends" Leaf Leaf))
+    (Node 30 "Harry" (Node 25 "Neil" Leaf Leaf) (Node 35 "Random Name" Leaf Leaf))
 
-lookup_test_1 :: Test
-lookup_test_1 = TestCase (assertEqual "Lookup Test" (Node 5 "Alex" Leaf Leaf) (bst_lookup 5 bst_constructor))
 
-basic_test :: Test
-basic_test = TestCase (assertEqual "Test that 5 is " 5 5)
-
-allTests :: Test
-allTests = TestList [basic_test]
+lookupTests :: Test
+lookupTests = TestList [
+  TestCase (assertEqual "Lookup Test" (Just "Alex") (bst_lookup 5 bst_constructor))]
