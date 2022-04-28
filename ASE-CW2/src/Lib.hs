84bd64f591bec2b740478d338eedefa88031e36a
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-04-28 18:22:46 +0100

    Implemented delete

--- ASE-CW2/src/Lib.hs
+++ ASE-CW2/src/Lib.hs
@@ -13,7 +13,6 @@ data BST item =
 bst_lookup :: Int -> BST item -> Maybe item
 bst_lookup soughtKey Leaf = Nothing
 bst_lookup soughtKey (Node key item leftChild rightChild)
-  -- | soughtKey == Leaf = Leaf -- Is this needed? 
   | soughtKey > key = bst_lookup soughtKey rightChild
   | soughtKey < key = bst_lookup soughtKey leftChild
   | otherwise = Just item -- If keys match
@@ -23,5 +22,27 @@ insert :: Int -> item -> BST item -> BST item
 insert insertKey value Leaf = Node insertKey value Leaf Leaf
 insert insertKey value (Node key item leftChild rightChild) 
 	| key == insertKey = Node key value leftChild rightChild
-	| key  < insertKey = Node key value leftChild (insert insertKey value rightChild)
-	| key  > insertKey = Node key value (insert insertKey value leftChild) rightChild
+	| key < insertKey = Node key value leftChild (insert insertKey value rightChild)
+	| key > insertKey = Node key value (insert insertKey value leftChild) rightChild
+
+
+delete :: int -> item -> BST item -> BST item
+delete Nil value Leaf = Nil
+delete (Node key item leftChild rightChild) delete_key  
+	| delete_key == key = delete_node (Node key item leftChild rightChild)
+	| delete_key < key = Node key item (delete leftChild delete_key) rightChild
+	| delete_key > key = Node leftChild key (delete rightChild delete_key)
+
+
+delete_node :: BST item -> BST item
+delete_node (Node key item Left Leaf) = Leaf
+delete_node (Node key item Leaf rightChild) = rightChild
+delete_node (Node key item leftChild Leaf) = leftChild
+delete_node (Node key item leftChild rightChild) = (Node key2 item leftChild rightChild)
+	where 
+		key2 = find_minimum_node rightChild
+
+
+find_minimum_node :: int => BST item -> BST item
+find_minimum_node (Node key item Leaf _) = key
+find_minimum_node (Node key item leftChild _) = find_minimum_node leftChild
